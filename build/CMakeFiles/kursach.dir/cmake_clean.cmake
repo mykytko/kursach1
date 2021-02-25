29c4dce32997a2d971af3381e5159bf40228110f
mykytko <nd235471@gmail.com>
2021-02-25 15:35:47 +0200

    initial commit

---
+++ build/CMakeFiles/kursach.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/kursach.dir/main.cpp.o"
+  "kursach"
+  "kursach.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/kursach.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ build/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /var/home/mykytko/Documents/kursach
+# Build directory: /var/home/mykytko/Documents/kursach/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
